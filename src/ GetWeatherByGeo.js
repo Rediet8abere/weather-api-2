11594b952ac80c28918a6e7cdfdd083ad1d5d262
Rediet8abere <redietaberegesesse@gmail.com>
2021-02-16 15:14:24 -0800

    adding travis

--- src/ GetWeatherByGeo.js
+++ src/ GetWeatherByGeo.js
@@ -1,6 +1,5 @@
 export default async function getWeatherByGeo(apikey, lat, lon) {
 
-  const apiKey = apikey
   const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}`
   const res = await fetch(path)
   const json = await res.json()
